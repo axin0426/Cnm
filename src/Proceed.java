ec3de9fdd55b5913c361105a1eb996a013008f37
axin0426 <86584544@qq.com>
2015-04-19 13:57:09 +0800

    init

---
+++ src/Proceed.java
@@ -0,0 +1,8 @@
+
+import org.ansj.splitWord.analysis.ToAnalysis;
+public class Proceed {
+	public static void main(String[] args) {
+		 String str = "你好，我叫信东睿！" ;
+		 System.out.println(ToAnalysis.parse(str));
+	}
+}
